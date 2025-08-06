1a187527e27ac13a98c1181be09b550c76de4f5f
KevinEbed <133675857+KevinEbed@users.noreply.github.com>
2025-08-06 04:49:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ if st.session_state.cart:
                         st.markdown(f"- ⏰ **Time:** `{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}`")
                         st.session_state.cart = {}
                         st.session_state.checkout_in_progress = False
-                        st.experimental_rerun()
+                        st.rerun()
                     else:
                         st.session_state.checkout_in_progress = False
         except Exception as outer_e:
